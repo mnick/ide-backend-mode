1720b3ef536743153498231fbea8b4a88f7e4ac2
Chris Done <chrisdone@gmail.com>
2015-03-22 18:46:28 +0100

    Support showing type and goto-def

--- ide-backend-mode.el
+++ ide-backend-mode.el
@@ -21,6 +21,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Imports
 
+(require 'haskell-mode)
 (require 'haskell-cabal)
 (require 'cl-lib)
 (require 'fifo)
@@ -32,6 +33,9 @@
   "A minor mode enabling various features based on ide-backend."
   :lighter " IDE"
   :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "M-.") 'ide-backend-mode-goto)
+            (define-key map (kbd "C-c C-k") 'ide-backend-mode-clear)
+            (define-key map (kbd "C-c C-t") 'ide-backend-mode-type)
             map))
 
 (define-derived-mode inferior-ide-backend-mode fundamental-mode "Inferior-IDE"
@@ -120,8 +124,9 @@ the minor mode when it is started, but can be overriden."
 (defun ide-backend-mode-clear ()
   "Clear the interaction buffer."
   (interactive)
-  (let ((inhibit-read-only t))
-    (erase-buffer)))
+  (with-current-buffer (ide-backend-mode-buffer)
+    (let ((inhibit-read-only t))
+      (erase-buffer))))
 
 (defun ide-backend-mode-load ()
   "Load the current buffer's file."
@@ -131,6 +136,64 @@ the minor mode when it is started, but can be overriden."
     (with-current-buffer (ide-backend-mode-buffer)
       (ide-backend-mode-update-file
        (file-relative-name filename default-directory)))))
+
+(defun ide-backend-mode-goto ()
+  "Go to definition of thing at point."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (module-name (haskell-guess-module-name))
+        (span (ide-backend-mode-span)))
+    (let* ((infos (ide-backend-mode-get-span-info
+                   module-name
+                   (with-current-buffer (ide-backend-mode-buffer)
+                     (file-relative-name filename default-directory))
+                   span))
+           (info (car (mapcar #'identity (cdr (assoc 'info infos)))))
+           (id-info (cdr (assoc 'idInfo info)))
+           (scope (cdr (assoc 'scope (cdr (assoc 'scope id-info)))))
+           (prop (cdr (assoc 'prop id-info))))
+      (cond
+       ((string= scope "local")
+        (let* ((def-span (cdr (assoc 'defSpan prop)))
+               (points (ide-backend-mode-points-from-span def-span)))
+          (goto-char (car points))))
+       (t
+        (let* ((package (cdr (assoc 'package (cdr (assoc 'definedIn prop)))))
+               (module (cdr (assoc 'name (cdr (assoc 'definedIn prop)))))
+               (name-ver (cdr (assoc 'packageKey package))))
+          (message "Imported from %s (%s)"
+                   module
+                   name-ver)))))))
+
+(defun ide-backend-mode-type ()
+  "Display type info of thing at point."
+  (interactive)
+  (let* ((filename (buffer-file-name))
+         (module-name (haskell-guess-module-name))
+         (points (ide-backend-mode-points))
+         (span (ide-backend-mode-span-from-points (car points)
+                                                  (cdr points))))
+    (let* ((info (ide-backend-mode-get-exp-types
+                  module-name
+                  (with-current-buffer (ide-backend-mode-buffer)
+                    (file-relative-name filename default-directory))
+                  span))
+           (types (mapcar #'identity (cdr (assoc 'info info)))))
+      (unless (null types)
+        (message
+         "%s"
+         (haskell-fontify-as-mode
+          (mapconcat
+           (lambda (type)
+             (concat
+              (buffer-substring-no-properties
+               (car points)
+               (cdr points))
+              " :: "
+              (cdr (assoc 'type type))))
+           (list (car (reverse types)))
+           "\n")
+          'haskell-mode))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Process filters and sentinel
@@ -148,10 +211,17 @@ the minor mode when it is started, but can be overriden."
                 (ide-backend-mode-log
                  "<- %s"
                  (haskell-fontify-as-mode line 'javascript-mode))
-                (unless (condition-case e
-                            (funcall cont data (json-read-from-string line))
-                          (error (message "Haskell process command errored with: %S" e)
-                                 t))
+                (when (let ((ret (condition-case e
+                                     (funcall cont data (json-read-from-string line))
+                                   (error (message "Haskell process command errored with: %S" e)
+                                          t))))
+                        (ecase ret
+                          (:done t)
+                          (:continue nil)
+                          (t
+                           (error "A command handler must return either :done or :continue,
+but it returned: %S
+command was: %S" ret ide-backend-mode-current-command))))
                   (cl-loop for line in lines
                            do (ide-backend-mode-log
                                "Extraneous lines after command completed: %s"
@@ -193,6 +263,32 @@ DATA line) for each response line until CONT returns nil."
   (fifo-push (ide-backend-mode-queue)
              (list :json json :data data :cont cont))
   (ide-backend-mode-queue-trigger))
+
+(defun ide-backend-mode-call (json)
+  "Call a JSON command. Wait for any existing queued commands to
+complete, then sends the request, blocking on the
+response. Returns the response."
+  (let ((data (list nil)))
+    (ide-backend-mode-enqueue
+     json data
+     (lambda (data reply)
+       (setcar data reply)
+       :done))
+    (ide-backend-mode-queue-flush)
+    (car-safe data)))
+
+(defun ide-backend-mode-queue-processed-p ()
+  "Return t if command queue has been completely processed."
+  (and (fifo-null-p ide-backend-mode-queue)
+       (null ide-backend-mode-current-command)))
+
+(defun ide-backend-mode-queue-flush ()
+  "Block till PROCESS's command queue has been completely processed.
+This uses `accept-process-output' internally."
+  (let ((proc (ide-backend-mode-process)))
+    (while (not (ide-backend-mode-queue-processed-p))
+      (ide-backend-mode-queue-trigger)
+      (accept-process-output proc 1))))
 
 (defun ide-backend-mode-queue-trigger ()
   "Trigger the next command in the queue if there is no current
@@ -262,6 +358,38 @@ directory."
      nil
      'ide-backend-mode-loading-callback)))
 
+(defun ide-backend-mode-get-span-info (module file span)
+  "Get the span info of the given location."
+  (with-current-buffer (ide-backend-mode-buffer)
+    (ide-backend-mode-call
+     `((request . "getSpanInfo")
+       (module . ,module)
+       (span . ((filePath   . ,file)
+                (fromLine   . ,(plist-get span :sl))
+                (fromColumn . ,(plist-get span :sc))
+                (toLine     . ,(plist-get span :el))
+                (toColumn   . ,(plist-get span :ec))))))))
+
+(defun ide-backend-mode-get-exp-types (module file span)
+  "Get the type info of the given location."
+  (with-current-buffer (ide-backend-mode-buffer)
+    (ide-backend-mode-call
+     `((request . "getExpTypes")
+       (module . ,module)
+       (span . ((filePath   . ,file)
+                (fromLine   . ,(plist-get span :sl))
+                (fromColumn . ,(plist-get span :sc))
+                (toLine     . ,(plist-get span :el))
+                (toColumn   . ,(plist-get span :ec))))))))
+
+(defun ide-backend-mode-get-use-sites (module file span)
+  "Get all uses of an identifier."
+  )
+
+(defun ide-backend-mode-get-completions (module string)
+  "Get all uses of an identifier."
+  )
+
 (defun ide-backend-mode-loading-callback (_ reply)
   "Callback for when loading modules."
   (cond
@@ -273,13 +401,13 @@ directory."
                (propertize msg 'face 'bold)
                (propertize (format "(%d of %d)" step steps)
                            'face 'font-lock-comment-face)))
-    t)
+    :continue)
    (t
     (ide-backend-mode-enqueue
      `((request . "getSourceErrors"))
      nil
      'ide-backend-mode-get-source-errors-callback)
-    nil)))
+    :done)))
 
 (defun ide-backend-mode-get-source-errors-callback (_ reply)
   "Handle the reply from getting source errors."
@@ -316,7 +444,41 @@ directory."
         (message (propertize "OK (%d warning%s)." 'face 'compilation-warning)
                  warnings
                  (if (= 1 warnings) "" "s")))))
-  nil)
+  :done)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Span functions
+
+(defun ide-backend-mode-points ()
+  "Get the current points; either a selected region or an
+identifier's points."
+  (if (region-active-p)
+      (cons (region-beginning) (region-end))
+    (let ((ident (haskell-ident-pos-at-point)))
+      (cons (car ident)
+            (cdr ident)))))
+
+(defun ide-backend-mode-span-from-points (beg end)
+  "Get the span representation for the span from BEG to END."
+  (save-excursion
+    (list :sl (progn (goto-char beg)
+                     (line-number-at-pos))
+          :sc (1+ (current-column))
+          :el (progn (goto-char end)
+                     (line-number-at-pos))
+          :ec (1+ (current-column)))))
+
+(defun ide-backend-mode-points-from-span (span)
+  "Get buffer points from a span."
+  (save-excursion
+    (goto-char (point-min))
+    (forward-line (1- (cdr (assoc 'fromLine span))))
+    (forward-char (1- (cdr (assoc 'fromColumn span))))
+    (let ((beg (point)))
+      (goto-char (point-min))
+      (forward-line (1- (cdr (assoc 'toLine span))))
+      (forward-char (1- (cdr (assoc 'toColumn span))))
+      (cons beg (point)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; JSON helpers
